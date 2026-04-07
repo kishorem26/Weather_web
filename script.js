a2836a50b6dd57cd01df81ebb82e3a075015dc77
kishorem26 <kishorecm210@gmail.com>
2026-04-07 15:50:20 +0530

    first commit

--- script.js
+++ script.js
@@ -7,40 +7,29 @@ async function getWeather() {
   }
 
   try {
-    // loader show
     document.getElementById("loader").style.display = "block";
     document.getElementById("error").innerText = "";
 
-    // Step 1: get coordinates
-    const geoRes = await fetch(
-      `https://geocoding-api.open-meteo.com/v1/search?name=${city}`
-    );
-    const geoData = await geoRes.json();
+    const apiKey = "b90aaec436812d830bac7b95cfc5bb93";
 
-    if (!geoData.results) {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+
+    const data = await res.json();
+
+    if (data.cod !== 200) {
       document.getElementById("error").innerText = "City not found";
       document.getElementById("loader").style.display = "none";
       return;
     }
 
-    const lat = geoData.results[0].latitude;
-    const lon = geoData.results[0].longitude;
-
-    // Step 2: get weather
-    const weatherRes = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`
-    );
-    const data = await weatherRes.json();
-
-    // show data
-    document.getElementById("cityName").innerText = city;
-    document.getElementById("temp").innerText = data.current_weather.temperature;
-    document.getElementById("wind").innerText = data.current_weather.windspeed;
-    document.getElementById("humidity").innerText = "--"; // open-meteo no humidity here
+    document.getElementById("cityName").innerText = data.name;
+    document.getElementById("temp").innerText = data.main.temp;
+    document.getElementById("wind").innerText = data.wind.speed;
+    document.getElementById("humidity").innerText = data.main.humidity;
 
     document.getElementById("weatherCard").style.display = "block";
-
-    // hide loader
     document.getElementById("loader").style.display = "none";
 
   } catch (err) {
